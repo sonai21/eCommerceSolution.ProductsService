d2be4b614699ba0f08cea5adfb064b36ed5e776b
alangkrita <alangkrita.sarkar@seaker.ai>
2026-05-10 18:13:26 +0530

    Configure PostgresSql and Entity FrameWork Core

---
+++ eCommerceSolution.ProductsService/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using eCommerceSolution.ProductsService.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSolution.ProductsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+
+    }
+}
---
+++ eCommerceSolution.ProductsService/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerceSolution.ProductsService.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSolution.ProductsService.DbContext;
+
+public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
