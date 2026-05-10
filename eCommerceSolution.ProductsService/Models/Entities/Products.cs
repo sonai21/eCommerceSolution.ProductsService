cc7a80cf1da58d1b1a110cb6c4ab50c1f9746742
alangkrita <alangkrita.sarkar@seaker.ai>
2026-05-10 15:23:17 +0530

    Define product Entity

--- eCommerceSolution.ProductsService/Models/Entities/Products.cs
+++ eCommerceSolution.ProductsService/Models/Entities/Products.cs
@@ -1,6 +1,10 @@
-﻿namespace eCommerceSolution.ProductsService.Models.Entities
+﻿namespace eCommerceSolution.ProductsService.Models.Entities;
+
+public class Products
 {
-    public class Products
-    {
-    }
+    public Guid ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public string? Category { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public int? QuantityInStock {  get; set; } 
 }
